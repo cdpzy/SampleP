3faa409f04565e6d2435d8632d1546c4d9c43ad9
pzy <pzy@ximi.com>
2016-05-11 15:22:32 +0800

    调整场景

--- sceneConfig/fishjoy_scenepath_2.lua
+++ sceneConfig/fishjoy_scenepath_2.lua
@@ -1,12 +1,12 @@
 local fishjoy_scenepath_2 = {
 	-- 顶部小
-	{t = 1, starts = {x=400, y=1230}, ends = {x=1520, y=1230}, duration = 15, section=-1, sct={x=400,y=660}, dct={x=1520,y=660},},
+	{t = 1, starts = {x=200, y=790}, ends = {x=1080, y=790}, duration = 15, section=-1, sct={x=230,y=380}, dct={x=1050,y=380},},
 	-- 顶部大
-	{t = 2, starts = {x=2010, y=1380}, ends = {x=-90, y=1380}, duration = 15, section=-1, sct={x=2020,y=480}, dct={x=-100,y=480},},
+	{t = 2, starts = {x=1300, y=790}, ends = {x=-20, y=790}, duration = 15, section=-1, sct={x=1016,y=290}, dct={x=264,y=290},},
 	-- 底部小
-	{t = 1, starts = {x=1520, y=-150}, ends = {x=400, y=-150}, duration = 15, section=-1, sct={x=1520,y=420}, dct={x=400,y=420},},
+	{t = 1, starts = {x=1080, y=-30}, ends = {x=200, y=-30}, duration = 15, section=-1, sct={x=1050,y=380}, dct={x=230,y=380},},
 	-- 底部大
-	{t = 2, starts = {x=-90, y=-300}, ends = {x=2010, y=-300}, duration = 15, section=-1, sct={x=-100,y=600}, dct={x=2020,y=600},},
+	{t = 2, starts = {x=-20, y=-30}, ends = {x=1300, y=-30}, duration = 15, section=-1, sct={x=264,y=470}, dct={x=1016,y=470},},
 	-- 每条路径鱼的数量，每条鱼落后的点数
 	{ c = 40, d = 20}
 }
