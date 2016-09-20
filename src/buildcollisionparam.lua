eb602eee5c74a6e4f16ff3eaf977167924d937e1
pzy <pzy@ximi.com>
2016-09-20 15:29:04 +0800

    add 32# 33# fish.

--- src/buildcollisionparam.lua
+++ src/buildcollisionparam.lua
@@ -83,81 +83,120 @@ end
 
 
 local FishConf = {
-	{ name = "fish_3_3",
-	  size = {450, 186},
-	  ellipse = { --width, height, posx, posy
-	  	{425, 105, 17, 40},
-	  	{85, 186, 178, 0},
-	  },
-	},
-    { name = "fish_2_8",
-      size = {300, 140},
-      ellipse = { --width, height, posx, posy
-        {290, 63, 3, 34},
-        {48, 108, 250, 16},
-        {34, 136, 184, 1},
-      },
+  {
+    name = "fish_33",
+    size= {247, 270},
+    ellipse = { --width, height, posx, posy
+      {244,144,1,63},
+      {167,259,4,7},
     },
-    { name = "fish_2_5",
-      size = {134, 168},
-      ellipse = { --width, height, posx, posy
-        {95, 73, 39, 45},
-        {50, 169, 67, 1},
-        {52, 16, 1, 73}, 
-      },
+  },
+  {
+    name = "fish_32",
+    size= {593, 268},
+    ellipse = { --width, height, posx, posy
+      {255,144,335,36},
+      {226,153,13,32},
+      {237,61,178,90},
     },
-    { name = "fish_2_3",
-      size = {128, 72},
-      ellipse = { --width, height, posx, posy
-        {128, 47, 0, 13},
-        {61, 71, 62, 1},
-      },
+  },
+  {
+    name = "fish_32",
+    size= {593, 268},
+    ellipse = { --width, height, posx, posy
+      {255,144,335,36},
+      {226,153,13,32},
+      {237,61,178,90},
     },
-    { name = "fish_2_2",
-      size = {176, 70},
+  },
+	{ name = "fish_31",
+      size = {480, 280},
       ellipse = { --width, height, posx, posy
-        {88, 44, 87, 11},
-        {83, 68, 3, 1},
-      },
-    },
-    { name = "fish_1_10",
-      size = {112, 121},
-      ellipse = { --width, height, posx, posy
-        {110, 56, 2, 31},
-        {38, 110, 41, 4},
-        {32, 94, 2, 17},
-      },
-    },
-    { name = "fish_1_6",
-      size = {63, 46},
-      ellipse = { --width, height, posx, posy
-        {63, 40, 1, 2},
-      },
-    },
-    { name = "fish_1_5",
-      size = {160, 70},
-      ellipse = { --width, height, posx, posy
-        {157, 47, 1, 9},
-      },
-    },
-    { name = "fish_1_4",
-      size = {120, 58},
-      ellipse = { --width, height, posx, posy
-        {111, 32, 2, 14},
+        {252,278,13,14},
       },
     }, 
-    { name = "fish_1_3",
-      size = {62, 41},
+	{ name = "fish_30",
+      size = {480, 280},
       ellipse = { --width, height, posx, posy
-        {64,39,1,2},
+        {387,137,74,45},
+        {115,178,30,8},
+      },
+    }, 
+	{ name = "fish_24",
+	  size = {396, 246},
+	  ellipse = { --width, height, posx, posy
+	  	{356, 122, 33, 31},
+	  	{53, 179, 216, 0},
+	  },
+	},
+    { name = "fish_18",
+      size = {315, 172},
+      ellipse = { --width, height, posx, posy
+        {294, 78, 18, 23},
+        {48, 141, 191, 16},
+        {52, 103, 262, 18},
       },
     },
-    { name = "fish_1_2",
-      size = {52, 24},
+    { name = "fish_15",
+      size = {149, 200},
       ellipse = { --width, height, posx, posy
-        {49,23,2,0},
+        {101, 71, 48, 47},
+        {59, 169, 78, 0},
+        {52, 17, 13, 73}, 
       },
-    },            
+    },
+    { name = "fish_13",
+      size = {141, 104},
+      ellipse = { --width, height, posx, posy
+        {128, 47, 12, 13},
+        {63, 75, 71, 0},
+      },
+    },
+    { name = "fish_12",
+      size = {189, 99},
+      ellipse = { --width, height, posx, posy
+        {91, 45, 98, 10},
+        {84, 67, 16, 1},
+      },
+    },
+    { name = "fish_10",
+      size = {128, 153},
+      ellipse = { --width, height, posx, posy
+        {114, 60, 14, 29},
+        {42, 122, 53, 0},
+        {40, 90, 11, 14},
+      },
+    },
+    { name = "fish_6",
+      size = {78, 76},
+      ellipse = { --width, height, posx, posy
+        {63, 40, 15, 0},
+      },
+    },
+    { name = "fish_5",
+      size = {174, 101},
+      ellipse = { --width, height, posx, posy
+        {157, 55, 18, 4},
+      },
+    },
+    { name = "fish_4",
+      size = {134, 82},
+      ellipse = { --width, height, posx, posy
+        {116, 45, 17, 12},
+      },
+    }, 
+    { name = "fish_3",
+      size = {77, 73},
+      ellipse = { --width, height, posx, posy
+        {63,39,14,1},
+      },
+    },
+    { name = "fish_2",
+      size = {55, 36},
+      ellipse = { --width, height, posx, posy
+        {54,20,3,2},
+      },
+    },        
 }
 
 function getCenter(L,H)
@@ -203,7 +242,6 @@ for _, fish in ipairs(FishConf) do
     if ellipse[1] == ellipse[2] then
         ellipse[1] = ellipse[1] + 1
     end
-
 		local center1, center2 = getCenter(ellipse[1], ellipse[2])
 
 		changeAnchor(fish.size, ellipse, center1)
@@ -226,14 +264,27 @@ for _, fish in ipairs(FishConf) do
 end
 
 --dump(outEllipse, "ellipse", 10)
+function string.split(input, delimiter)
+    input = tostring(input)
+    delimiter = tostring(delimiter)
+    if (delimiter=='') then return false end
+    local pos,arr = 0, {}
+    -- for each divider found
+    for st,sp in function() return string.find(input, delimiter, pos, true) end do
+        table.insert(arr, string.sub(input, pos, st - 1))
+        pos = sp + 1
+    end
+    table.insert(arr, string.sub(input, pos))
+    return arr
+end
 
 local file = io.open("fish_ellipse.lua", "wb")
 file:write("--autogen fish collision zone begin.  !!!DO NOT MODIFY!!! COPY&&PAST\n")
 file:write("local czConfig = {\n")
 for _,v in ipairs(outEllipse) do
-	file:write("\t{\n\t\t\"")
-	file:write(v[1])
-	file:write("\",\n")
+	local a = string.split(v[1], "_")
+	file:write(("\t[%d] = { -- %s\n"):format(tonumber(a[2]), v[1]))
+
 	for _,e in ipairs(v[2]) do
 		file:write("\t\t{")
 		for _,p in ipairs(e) do
